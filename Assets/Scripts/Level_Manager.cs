1e5df23766df30028cd5954fb008700189ad8822
Jose <al270748@uji.es>
2018-12-26 20:31:50 +0100

    Menu Principal

---
+++ Assets/Scripts/Level_Manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_Manager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void CargaNivel(string nombreNivel){
+		SceneManager.LoadScene (nombreNivel);
+	}
+	public void Salir () {
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/Salir.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour {
+
+	// Hace que salgamos del juego
+	void Salimos () {
+		Application.Quit();
+		Debug.Log("Salimos.");
+	}
+}
