299251aeb2f9e638246c1de601b9eafb3535ca55
SergioCG95 <al286319@uji.es>
2018-12-28 01:20:03 +0100

    penes

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class BossMovement : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    //PlayerHealth playerHealth;      // Reference to the player's health.
+    //EnemyHealth enemyHealth;        // Reference to this enemy's health.
+    
+    public float distancia = 1f;
+    Animator anim;
+
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        // If the enemy and the player have health left...
+        //if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        //{
+        // ... set the destination of the nav mesh agent to the player.
+       
+        //    }
+        //    // Otherwise...
+        //    else
+        //    {
+        //        // ... disable the nav mesh agent.
+        //        nav.enabled = false;
+        //    }
+
+
+        if (Vector3.Distance(transform.position, player.position) < distancia)
+        {
+            anim.SetBool("EstaCerca", true);
+            anim.SetBool("isIdle", false);
+        }
+        else
+        {
+            anim.SetBool("EstaCerca", false);
+            anim.SetBool("isIdle", true);
+        }
+    }
+}
